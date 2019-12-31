cdf481fea24084a597f75d6f1685a044d27b0f4b
Theodore Pantazopoulos <teopanta1986@gmail.com>
2019-12-31 12:42:15 +0200

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -33,6 +33,9 @@ fetch('https://danger-button-backend.herokuapp.com/api/users/signin', {
   method: 'post',
   headers: {
     'Access-Control-Allow-Origin':'*',
+    "Access-Control-Allow-Credentials":"true",
+    "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
+    "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
     'Accept': 'application/json',
   },
   body: JSON.stringify(

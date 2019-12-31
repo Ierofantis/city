f1817bd69b78d9ce8f7a2d7697c553eaeb8f3800
Theodore Pantazopoulos <teopanta1986@gmail.com>
2019-12-31 21:28:47 +0200

    Update TheButton.js

--- src/TheButton.js
+++ src/TheButton.js
@@ -88,8 +88,11 @@ async  send() {
     method: "POST",
     body: JSON.stringify(subscription),
     headers: {
-      "content-type": "application/json",
-      'Access-Control-Allow-Origin': '*'
+      'Access-Control-Allow-Origin':'*',
+      'Access-Control-Allow-Credentials':'true',
+      'Access-Control-Allow-Methods': 'GET,HEAD,OPTIONS,POST,PUT',
+      'Access-Control-Allow-Headers': 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers',
+      'Accept': 'application/json',
     }
   });
   console.log("Push Sent...");
